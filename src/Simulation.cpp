51dc877070b0e0398a9862020f5cc03c98a25762
itay mesika <itaymessika@gmail.com>
2024-11-22 17:25:41 +0200

    settlement complited

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -1,7 +1,32 @@
 #include "Simulation.h"
 #include <iostream>
+using std::string;
 
-Simulation::Simulation(const string &configFilePath):isRunning(false),planCounter(0){
-    std::cout << "fsfaf" << std::endl;
+Simulation::Simulation(const string &config_file_path):
+isRunning(false),planCounter(0){
+    std::cout<< "Simulation started" << std::endl;
 }
+
+void start();
+
+void addPlan(const Settlement &settlement, SelectionPolicy *selectionPolicy);
+
+void addAction(BaseAction *action);
+
+bool addSettlement(Settlement settlement);
+
+bool addFacility(FacilityType facility);
+
+bool isSettlementExists(const string &settlementName);
+
+Settlement &getSettlement(const string &settlementName);
+
+Plan &getPlan(const int planID);
+
+void step();
+
+void close();
+
+void open();
+
 
