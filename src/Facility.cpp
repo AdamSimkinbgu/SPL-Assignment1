203c397c0a2bfaecae13899ec0f7ecde9c3a17de
itay mesika <itaymessika@gmail.com>
2024-11-22 17:58:03 +0200

    facilitytype done

---
+++ src/Facility.cpp
@@ -0,0 +1,33 @@
+#pragma once
+#include "Facility.h"
+#include <iostream>
+using std::string;  
+
+FacilityType::FacilityType(const string &name, const FacilityCategory category, 
+const int price, const int lifeQuality_score, const int economy_score, const int environment_score):
+    name(name), category(category), price(price), lifeQuality_score(lifeQuality_score), 
+    economy_score(economy_score), environment_score(environment_score){};
+
+const string &FacilityType::getName() const{
+    return name;
+};
+int FacilityType::getCost() const{
+    return price;
+};
+
+int FacilityType::getLifeQualityScore() const{
+    return lifeQuality_score;
+}
+
+int FacilityType::getEnvironmentScore() const{
+    return environment_score;
+}
+
+int FacilityType::getEconomyScore() const{
+    return economy_score;
+}
+
+FacilityCategory FacilityType::getCategory() const{
+    return category;
+}
+
