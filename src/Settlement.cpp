6235632a5bcca38d62d38a470ff96fc0311ab6fa
Adam Simkin <simkinad@post.bgu.ac.il>
2024-11-23 16:47:33 +0200

    new skeleton imported

--- src/Settlement.cpp
+++
@@ -1,7 +0,0 @@
-#include <Settlement.h>
-#include <iostream>
-
-Settlement::Settlement(const string &name, SettlementType type) {}
-const string &Settlement::getName() const {}
-SettlementType Settlement::getType() const {}
-const string Settlement::toString() const {}
